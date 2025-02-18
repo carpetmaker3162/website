25bc8b59510e4d4c7c930959f70813243df53868
carpetmaker3162 <domesticatedhuman9999@gmail.com>
2025-02-17 19:35:45 -0500

    pretty empty for now

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">home</a>
+	<a href="/contact">contact</a>
+</nav>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Neng Li</h1>
+<p>This is my website</p>
